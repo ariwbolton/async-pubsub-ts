0fceae96a09f69c6ff1ed83a84019acf133becc3
Ari Wolff Bolton <ariwbolton@gmail.com>
2023-11-12 11:23:55 -0800

    Fix a couple of things

--- src/AsyncEventEmitter.ts
+++ src/AsyncEventEmitter.ts
@@ -1,50 +1,57 @@
 
-type Handler<Event> = (e: Event) => Promise<void>
-type HandlerReg<EventTypes extends Record<string, any>, EventName extends keyof EventTypes> = {
-    event: EventName
-    handler: Handler<EventTypes[EventName]>
-}
+type Handler<Event> = (e: Event) => Promise<any>
+type BoxedEvent<EventTypes extends Record<string, any>> = {
+    [K in keyof EventTypes]: { name: K, event: EventTypes[K] }
+}[string]
 
 export class AsyncEventEmitterBuilder<
-    EventTypes extends Record<string, any>,
-    EventHandlers extends readonly HandlerReg<EventTypes, any>[]
+    EventTypes extends Record<string, any>
 > {
-    constructor(private readonly events: string[], private readonly handlers: EventHandlers) {}
+    constructor(private readonly events: string[], private handlers: { [E in keyof EventTypes]: Handler<EventTypes[E]>[] }) {}
 
     static start() {
-        return new AsyncEventEmitterBuilder<{}, []>([], [])
+        return new AsyncEventEmitterBuilder<{}>([], {})
     }
 
     define<EventType, EventName extends string>(event: EventName):
-        AsyncEventEmitterBuilder<
-            EventTypes & { [K in EventName]: EventType },
-            EventHandlers
-        >
+        AsyncEventEmitterBuilder<EventTypes & { [K in EventName]: EventType }>
     {
         return new AsyncEventEmitterBuilder([...this.events, event], this.handlers)
     }
 
     on<
-        EventName extends keyof EventTypes,
-        H extends Handler<EventTypes[EventName]>
-    >(event: EventName, handler: H) {
-        const eventReg: HandlerReg<EventTypes, EventName> = {
-            event,
-            handler
+        E extends keyof EventTypes,
+        H extends Handler<EventTypes[E]>
+    >(name: E, handler: H) {
+        if (!this.handlers[name]) {
+            this.handlers[name] = []
         }
 
-        return new AsyncEventEmitterBuilder<EventTypes, [...EventHandlers, typeof eventReg]>(this.events, [...this.handlers, eventReg])
+        this.handlers[name].push(handler)
+
+        return this
     }
 
-    build() {}
+    build() {
+        return new AsyncEventEmitter<EventTypes>(this.handlers)
+    }
 }
 
-export class AsyncEventEmitter<T> {
-    constructor() {}
+export class AsyncEventEmitter<EventTypes extends Record<string, any>> {
+    private queue: BoxedEvent<EventTypes>[] = []
 
-    emit() {}
+    constructor(private readonly handlers: Record<string, Handler<any>[]>) {}
+
+    emit<N extends keyof EventTypes, E extends EventTypes[N]>(name: N, event: E) {
+        this.queue.push({ name, event } as BoxedEvent<EventTypes>)
+    }
 }
 
 const builder = AsyncEventEmitterBuilder.start()
-    .define<{ something: 'here' }, 'event1'>('event1')
-    .define<{ else: 2 }, 'event2'>('event2')
+    .define<{ something: 'here' }, '1'>('1')
+    .define<{ else: 2 }, '2'>('2')
+    .on('1', async (e) => null)
+    .define<{another: 'hi'}, '3'>('3')
+    .on('2', async (e) => null)
+
+const emitter = builder.build()
